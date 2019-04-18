09bb056086eaff50ade31833c7dec56e5345c411
arashidi21 <48811255+arashidi21@users.noreply.github.com>
2019-04-18 18:38:39 -0400

    Game Master and Player Controller
    Game Master only changes score text when you collide with coin. Player Controller now initializes this game master script and Greg's Player Stats.

---
+++ GameMaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    //Ali Rashidinejad
+    //counts points and sets coinText equal to points
+    public int points;
+
+    public Text coinText;
+
+    void Update()
+    {
+        coinText.text = "Points: " + points;
+    }
+}
