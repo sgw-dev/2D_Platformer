fb83a978df5c7883f4935cd28773e3c99d872a5c
Spencer Burke <sburke17@emich.edu>
2019-10-27 20:42:59 -0400

    Player Movement & Attack

---
+++ Assets/Script/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour{
+
+    public int maxHealth;
+    private int health;
+
+    public GameObject healthbar;
+    private float healthWidth;
+    private float healthScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        healthScale = healthbar.transform.localScale.x;
+        healthWidth = healthScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void applyDamage(int damage) {
+        health -= damage;
+        if (health <= 0)
+        {
+            die();
+        }
+        float ratio = (float)damage / healthWidth;
+        healthScale = healthbar.transform.localScale.x;
+        healthbar.transform.localScale = new Vector3 (healthScale - (healthWidth*ratio), healthbar.transform.localScale.y, 1.0f);
+
+    }
+    public void die() {
+        Destroy(gameObject);
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,7 +9,11 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;//the rigid body attached to the gameobject
     public float speed; //how much force is added each time
     public float maxSpeed; //how fast you are alowed to go
+    public float sprint;
     public float verticalSpeed;//how strong you jumps are
+    public float attackTime;
+    private float attackTimer = 0.0f;
+    public Collider2D attackCollider;
 
     public GameObject stage;
     public GameObject bottom;
@@ -20,6 +24,7 @@ public class Player : MonoBehaviour
     private Transform leftT;
 
     public LayerMask collisionMask;
+    public LayerMask hitMask;
 
     public float checkDistance;
     private int numJumps = 1;
@@ -46,23 +51,54 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-        
 
-        //if D pressed
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetButtonDown("Fire3")) {
+            Debug.Log("Fire3"); 
+        }/*
+        if (Input.GetButtonUp("Fire3"))
+        {
+            maxSpeed -= sprint;
+        }*/
+        //if D pressed and sprint
+        if (Input.GetAxisRaw("Horizontal") >0.0f && Input.GetButton("Fire3")
+            || Input.GetButton("Fire3") && Input.GetAxisRaw("Horizontal") > 0.0f) {
             //if the current x velocity is less that the max alowed
-            if(rb.velocity.x < maxSpeed)
+            if (rb.velocity.x < (maxSpeed + sprint))
             {
                 //add force to the object to the right
                 rb.AddForce(new Vector2(speed, 0.0f));
                 //flip the sprite to face right
                 this.GetComponent<SpriteRenderer>().flipX = false;
-                
             }
-            
+        }
+        //if D pressed
+        if (Input.GetAxisRaw("Horizontal") > 0.0f )
+        {
+            //if the current x velocity is less that the max alowed
+            if (rb.velocity.x < maxSpeed)
+            {
+                //add force to the object to the right
+                rb.AddForce(new Vector2(speed, 0.0f));
+                //flip the sprite to face right
+                this.GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+        //if A pressed and sprint
+        if (Input.GetAxisRaw("Horizontal") < 0.0f && Input.GetButton("Fire3")
+            || Input.GetButton("Fire3") && Input.GetAxisRaw("Horizontal") <  0.0f)
+        {
+            //is the current x velocity is greater than negative max alowed
+            //this is becuase you can be going fast backwards too
+            if (rb.velocity.x > -(maxSpeed + sprint))
+            {
+                //add force to the object to the left
+                rb.AddForce(new Vector2(-speed, 0.0f));
+                //flip the sprite to face left
+                this.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
         //if A pressed
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetAxisRaw("Horizontal") < 0.0f)
         {
             //is the current x velocity is greater than negative max alowed
             //this is becuase you can be going fast backwards too
@@ -72,15 +108,13 @@ public class Player : MonoBehaviour
                 rb.AddForce(new Vector2(-speed, 0.0f));
                 //flip the sprite to face left
                 this.GetComponent<SpriteRenderer>().flipX = true;
-                
             }
-
         }
-        //if W pressed
-        if (Input.GetKeyDown(KeyCode.W))
+        
+        //if jump pressed
+        if (Input.GetButtonDown("Jump"))
         {
             //add force up
-            /*Needs more work to limit the jumps*/
             if (checkRight()) {
                 rb.AddForce(new Vector2(-(horizSpeed * 10), horizSpeed*5.5f));
             } else if (checkLeft())
@@ -91,13 +125,43 @@ public class Player : MonoBehaviour
                 rb.AddForce(new Vector2(0.0f, verticalSpeed*10));
                 numJumps--;
             }
-
-           
-            
-
+        }
+        attackTimer += Time.deltaTime;
+        //if Fire1 pressed
+        if (Input.GetButtonDown("Fire1"))
+        {           
+            if (attackTimer > attackTime)
+            {
+                attack();
+                //Debug.Log("Attacked");
+                attackTimer = 0.0f;
+            }          
         }
 
     }
+    private void attack() {
+        Collider2D myCollider = attackCollider;
+        int numColliders = 10;
+        Collider2D[] colliders = new Collider2D[numColliders];
+        ArrayList names = new ArrayList();
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.layerMask = hitMask;
+        // Set you filters here according to https://docs.unity3d.com/ScriptReference/ContactFilter2D.html
+        int colliderCount = myCollider.OverlapCollider(contactFilter, colliders);
+        for (int i = 0; i < numColliders; i++) {
+            if (colliders[i] != null) {
+                if (colliders[i].tag.CompareTo("Enemy") == 0) {
+                    if (!names.Contains(colliders[i].name))
+                    {
+                        colliders[i].SendMessage("applyDamage", 1);
+                        names.Add(colliders[i].name);
+                    }
+                }
+                
+            }
+        }
+    }
+
     public bool checkRight() {
         RaycastHit2D hit = Physics2D.Raycast(rightT.transform.position, Vector2.right, checkDistance, collisionMask);
         if (hit.collider != null)
