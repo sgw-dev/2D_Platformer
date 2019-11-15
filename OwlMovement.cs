ae079c9e0acd30e1137bc0823f6af1933c217f0a
ParkerAllen <36487344+ParkerAllen@users.noreply.github.com>
2019-11-14 16:30:20 -0800

    Add files via upload

---
+++ OwlMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlMovement : FlyingEnemyPathing
+{
+
+    private Vector2 direction;
+
+    public void FixedUpdate()
+    {
+        rb.velocity = direction = Vector2.zero;
+        //direction = Avoid();
+        if (!needToAvoid)
+        {
+            if (lineOfSight())
+            {
+                direction = target;
+            }
+            else if(perch != Vector3.zero)
+            {
+                direction = Perch();
+            }
+            else
+            {
+                perch = FindGround();
+            }
+        }
+        if(direction != Vector2.zero)
+        {
+            Move(direction);
+        }
+        LockRotation();
+        Debug.DrawLine(transform.position, direction, Color.cyan);
+    }
+}
