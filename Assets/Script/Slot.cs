ab98b8f2e31e06ed6587457900205d1044f020d9
Spencer Burke <sburke17@emich.edu>
2020-03-19 14:38:31 -0400

    Can Show Description Box

--- Assets/Script/Slot.cs
+++ Assets/Script/Slot.cs
@@ -10,14 +10,36 @@ public class Slot : MonoBehaviour
     private Item item;
     private Transform location;
     public GameObject hoverPrefab;
+    private GameObject inventory;
+    private bool display;
+    private GameObject pane;
 
     public Slot()
     {
 
     }
+    public void Awake()
+    {
+        inventory = GameObject.Find("Canvas/Inventory/BoxesPanel");
+    }
     public void OnPointerClick(PointerEventData eventData) // 3
     {
-        print("I was clicked");
+        if (!display)
+        {
+            pane = Instantiate(hoverPrefab) as GameObject;
+            pane.transform.SetParent(inventory.transform);
+            pane.GetComponent<RectTransform>().localPosition = this.GetComponent<RectTransform>().localPosition + new Vector3(115, -55, 0);
+            display = true;
+        }
+        else
+        {
+            if(pane != null)
+            {
+                Destroy(pane);
+                pane = null;
+            }
+            display = false;
+        }
         
     }
 }
