4cbcbbd35cc3962ec411715aeb9b7aeeba13ff2d
Spencer Burke <sburke17@emich.edu>
2020-03-19 14:13:05 -0400

    Added Slot Script to all boxes in Inventory

--- Assets/Script/Slot.cs
+++ Assets/Script/Slot.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
+    , IPointerClickHandler // 2
 {
-    
+    private Item item;
+    private Transform location;
+    public GameObject hoverPrefab;
+
+    public Slot()
+    {
+
+    }
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        print("I was clicked");
+        
+    }
 }
