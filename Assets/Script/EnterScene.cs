39c31a904c13d72600ae14b35a54d14ceeb58cfa
Spencer Burke <sburke17@emich.edu>
2020-04-09 14:17:30 -0400

    Entering/Exiting Bear Den

---
+++ Assets/Script/EnterScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterScene : MonoBehaviour
+{
+    public bool ocupied;
+    public GameObject promptText;
+    public string levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ocupied)
+        {
+            promptText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+
+        }else if(!ocupied && promptText.activeSelf)
+        {
+            promptText.SetActive(false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            ocupied = true;
+        }
+       
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            ocupied = false;
+        }
+    }
+}
